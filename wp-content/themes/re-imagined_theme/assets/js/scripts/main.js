4d5c510a76201b39900193f7da95891a3f148586
Bekah Baker <bekahlbaker@gmail.com>
2016-07-21 12:49:22 -0500

    finishing touches

--- wp-content/themes/re-imagined_theme/assets/js/scripts/main.js
+++ wp-content/themes/re-imagined_theme/assets/js/scripts/main.js
@@ -1,8 +1,5 @@
 +function ($) {
     'use strict';
 
-  $(document).ready(function(){
-    $("#sticker").sticky({topSpacing:0});
-  });
 
 }(jQuery);
--- wp-content/themes/re-imagined_theme/single.php
+++ wp-content/themes/re-imagined_theme/single.php
@@ -2,7 +2,7 @@
 
 <div class="single_post container">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<div class="single content-block">	
+	<div class="single">	
 		<h3><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
 		<p class="description">by <?php the_author(); ?> | <?php echo get_the_date('l, F j, Y'); ?> | <?php the_category( ' ', $parents, $post_id ); ?> | <a href="<?php comment_link(); ?>"><?php comments_number(); ?></a></p>
 		
@@ -10,7 +10,7 @@
 			<div class="featured">
 				<?php the_post_thumbnail(); ?>
 			</div>
-			<div class="post_content">
+			<div class="post_content content-block">
 				<?php the_content(); ?>
 			</div>
 		</div>
